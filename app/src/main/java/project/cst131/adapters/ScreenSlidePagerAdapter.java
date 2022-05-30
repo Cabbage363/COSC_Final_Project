fc64c4bc36ec5c363a32ad699b9993c494ecd881
Cabbage363 <Cabbage363@hotmail.com>
2022-05-29 21:53:48 -0600

    New new x2

--- app/src/main/java/project/cst131/adapters/ScreenSlidePagerAdapter.java
+++ app/src/main/java/project/cst131/adapters/ScreenSlidePagerAdapter.java
@@ -1,12 +1,8 @@
 package project.cst131.adapters;
 
-import android.content.Context;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import project.cst131.fragments.ClassesFragment;
